aa49c27e78163fbd7e411e23b63c05fa708356be
Miguel Camacho <miguelangelcamachocampos@gmail.com>
2019-11-17 19:44:03 -0600

    Initial commit :rocket:

---
+++ app/src/main/java/com/example/login_register/DBContract.kt
@@ -0,0 +1,14 @@
+package com.example.login_register
+import android.provider.BaseColumns
+
+class DBContract {
+    /* Inner class that defines the table contents */
+    class UserEntry : BaseColumns{
+        companion object{
+            val TABLE_NAME = "users"
+            val COLUMN_USER_ID = "userid"
+            val COLUMN_NAME = "name"
+            val COLUMN_AGE = "age"
+        }
+    }
+}
---
+++ app/src/main/java/com/example/login_register/UserModel.kt
@@ -0,0 +1,3 @@
+package com.example.login_register
+
+class UserModel(val userid: String, val name: String, val age: String)
